0386c7eb9401a134c56b586fbc6c335d34f90bf6
enriquepaz1 <enrique.bayron.paz@gmail.com>
2021-08-19 00:18:12 -0400

    Task: 18/08/21 modules admin/home/buy => shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Directive1Directive } from './directive1.directive';
 import { Directive2Directive } from './directive2.directive';
+import { AdminModule } from './pages/admin/admin.module';
+import { BuyModule } from './pages/buy/buy.module';
+import { HomeModule } from './pages/home/home.module';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
@@ -21,7 +24,11 @@ import { Test3Component } from './test3/test3.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HomeModule,
+    AdminModule,
+    BuyModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
