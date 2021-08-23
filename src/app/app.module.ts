2710595fc1063e867e301394a00f393ee42af652
enriquepaz1 <enrique.bayron.paz@gmail.com>
2021-08-23 01:14:52 -0400

    Task: 18/08/21 Modules: App/Core/Layout  Architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { Directive1Directive } from './directive1.directive';
 import { Directive2Directive } from './directive2.directive';
+import { LayoutModule } from './layout/layout.module';
 import { AdminModule } from './pages/admin/admin.module';
 import { BuyModule } from './pages/buy/buy.module';
 import { HomeModule } from './pages/home/home.module';
@@ -21,7 +23,7 @@ import { Test3Component } from './test3/test3.component';
     Directive1Directive,
     Directive2Directive
   ],
-  imports: [BrowserModule, FormsModule, HomeModule, AdminModule, BuyModule],
+  imports: [BrowserModule, FormsModule, HomeModule, AdminModule, BuyModule,CoreModule,LayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent
+  ],
+
+  exports:[
+    HeaderComponent,
+    SidebarComponent
+
+  ]
+})
+export class LayoutModule { }
