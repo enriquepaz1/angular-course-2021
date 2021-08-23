cd7fac857a31b8d08055fdd63745be5d42cf399c
enriquepaz1 <enrique.bayron.paz@gmail.com>
2021-08-23 15:34:50 -0400

    5 files modified, 54 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { Directive1Directive } from './directive1.directive';
-import { Directive2Directive } from './directive2.directive';
-import { LayoutModule } from './layout/layout.module';
-import { AdminModule } from './pages/admin/admin.module';
-import { BuyModule } from './pages/buy/buy.module';
-import { HomeModule } from './pages/home/home.module';
-import { Test1Component } from './test1/test1.component';
-import { Test2Component } from './test2/test2.component';
-import { Test3Component } from './test3/test3.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component,
-    Test2Component,
-    Test3Component,
-    Directive1Directive,
-    Directive2Directive
-  ],
-  imports: [BrowserModule, FormsModule, HomeModule, AdminModule, BuyModule,CoreModule,LayoutModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
