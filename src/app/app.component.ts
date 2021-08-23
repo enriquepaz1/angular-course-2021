c3ee58bb6ba4a47d02f1b9d05f3c6d5d42d72511
enriquepaz1 <enrique.bayron.paz@gmail.com>
2021-08-23 19:51:34 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,41 +9,115 @@ import { filter, map } from 'rxjs/operators';
 })
 export class AppComponent {
   title: string = 'anddgular2021';
-  
 
-   allVacunados=
-   [
-    {name: "juan", age: 16, date: "2019-09-07T15:50+00Z", disease: true, vaccineType: "A", vaccined:0, doses: 0},
-    {name: "maria", age: 23, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "B", vaccined:1, doses: 2},
-    {name: "carla", age: 45, date: "2019-09-07T15:50+00Z", disease: true, vaccineType: "C", vaccined:0, doses: 0},
-    {name: "marco", age: 50, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "A", vaccined:1, doses: 1},
-    {name: "marta", age: 12, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "A", vaccined:0, doses: 0},
-    {name: "jorge", age: 36, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "C", vaccined:1, doses: 3},
-    {name: "maritza", age: 18, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "C", vaccined:0, doses: 0},
-    {name: "leonardo", age: 35, date: "2019-09-07T15:50+00Z", disease: true, vaccineType: "B", vaccined:0, doses: 0},
-    {name: "ramiro", age: 24, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "B", vaccined:1, doses: 2},
-    {name: "juana", age: 13, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "A", vaccined:0, doses: 0}
-    ]
+  allVacunados = [
+    {
+      name: 'juan',
+      age: 16,
+      date: '2019-09-07T15:50+00Z',
+      disease: true,
+      vaccineType: 'A',
+      vaccined: 0,
+      doses: 0
+    },
+    {
+      name: 'maria',
+      age: 23,
+      date: '2019-09-07T15:50+00Z',
+      disease: false,
+      vaccineType: 'B',
+      vaccined: 1,
+      doses: 2
+    },
+    {
+      name: 'carla',
+      age: 45,
+      date: '2019-09-07T15:50+00Z',
+      disease: true,
+      vaccineType: 'C',
+      vaccined: 0,
+      doses: 0
+    },
+    {
+      name: 'marco',
+      age: 50,
+      date: '2019-09-07T15:50+00Z',
+      disease: false,
+      vaccineType: 'A',
+      vaccined: 1,
+      doses: 1
+    },
+    {
+      name: 'marta',
+      age: 12,
+      date: '2019-09-07T15:50+00Z',
+      disease: false,
+      vaccineType: 'A',
+      vaccined: 0,
+      doses: 0
+    },
+    {
+      name: 'jorge',
+      age: 36,
+      date: '2019-09-07T15:50+00Z',
+      disease: false,
+      vaccineType: 'C',
+      vaccined: 1,
+      doses: 3
+    },
+    {
+      name: 'maritza',
+      age: 18,
+      date: '2019-09-07T15:50+00Z',
+      disease: false,
+      vaccineType: 'C',
+      vaccined: 0,
+      doses: 0
+    },
+    {
+      name: 'leonardo',
+      age: 35,
+      date: '2019-09-07T15:50+00Z',
+      disease: true,
+      vaccineType: 'B',
+      vaccined: 0,
+      doses: 0
+    },
+    {
+      name: 'ramiro',
+      age: 24,
+      date: '2019-09-07T15:50+00Z',
+      disease: false,
+      vaccineType: 'B',
+      vaccined: 1,
+      doses: 2
+    },
+    {
+      name: 'juana',
+      age: 13,
+      date: '2019-09-07T15:50+00Z',
+      disease: false,
+      vaccineType: 'A',
+      vaccined: 0,
+      doses: 0
+    }
+  ];
 
+  personas = {
+    vacunados: this.allVacunados.filter(p => p.vaccined),
+    noVacunados: this.allVacunados.filter(p => !p.vaccined)
+  };
 
-    personas ={
-     vacunados: this.allVacunados.filter(p=>p.vaccined),
-     noVacunados: this.allVacunados.filter(p=>!p.vaccined)
-
-    };
-
-    verificarVacunados(){
-      const enf=this.personas.vacunados.filter(p=>p.disease=true)
-      const noEnf=this.personas.noVacunados.filter(p=>p.disease=false)
-       return (enf.length+noEnf.length===0);     
-  
-    } 
-
-  vacunar(person,tipo){
-
+  verificarVacunados() {
+    const enf = this.personas.vacunados.filter(p => (p.disease = true));
+    const noEnf = this.personas.noVacunados.filter(p => (p.disease = false));
+    return enf.length + noEnf.length === 0;
   }
 
-}
-   
+  //vacunar(person,status){
+  //const estado= this.personas[status].findIndex(p=>p===person);
+  //this.personas[status].splice(estado,1);
+  //this.personas.vacunados.push({...estado,vacunado:true});
 
-  
+  //}
+}
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -9,16 +9,25 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
     >
       <p>Name:{{ name | uppercase }}</p>
       <p>Age:{{ age }}</p>
-      <p>Date:{{ date }}</p>
+      <p>Date:{{ date}}</p>
       <p>Disease:{{ disease }}</p>
       <p>VaccineType:{{ vaccineType }}</p>
       <p>Vaccined:{{ vaccined }}</p>
       <p>Doses:{{ doses }}</p>
-      <button>Vacunar</button>
+
+
+
+      <button  *ngIf= "!vaccined" (click)="onVacunar()"
+      
+      [disabled]="age<18">Vacunar</button>
+      
     </div>
   `
 })
 export class PersonComponent implements OnInit {
+
+
+  //age<18 && disease=true
   @Input() name: string;
   @Input() age: number;
   @Input() date: string;
