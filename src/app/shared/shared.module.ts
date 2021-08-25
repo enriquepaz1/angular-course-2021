0d7b4dc1df887fcdde579ced38db2cc28759d6eb
enriquepaz1 <enrique.bayron.paz@gmail.com>
2021-08-24 23:45:00 -0400

    1 file modified, 2 added, 6 deleted

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { Provider2Component } from './provider2/provider2.component';
 import { Shared1Component } from './shared1/shared1.component';
-import { Shared2Component } from './shared2/shared2.component';
-import { Shared3Component } from './shared3/shared3.component';
+import { Shared2Directive } from './shared2.directive';
+import { Shared3Pipe } from './shared3.pipe';
 @NgModule({
   imports: [
     CommonModule],
-  declarations: [Shared1Component,Shared2Component,Shared3Component],
+  declarations: [Shared1Component,Shared2Directive,Shared3Pipe],
    exports:[
-     Shared1Component,Shared2Component,Shared3Component
-   ]
+     Shared1Component,Shared2Directive,Shared3Pipe]
 
 })
 export class SharedModule { }
