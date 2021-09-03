ae4614ceaf57ce7bb16fa06d49c3de36d230c888
enriquepaz1 <>
2021-09-03 01:57:43 -0400

    Task: 01/09/21

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,10 @@ export const environment = {
   production: false,
   app:{
    apiBaseUrl: "https://facebook-angular-enrique-default-rtdb.firebaseio.com"
-
   }
 };
 
-/*
+/*///////////////////
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
