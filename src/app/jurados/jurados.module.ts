88b5bb29f15295a13e029afdcd06d3a4376af5df
enriquepaz1 <enrique.bayron.paz@gmail.com>
2021-09-08 13:01:05 -0400

    1 file modified, 15 added

---
+++ src/app/jurados/jurados.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Jurados1Component } from './jurados1/jurados1.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Jurados1Component]
+})
+export class JuradosModule { }
---
+++ src/app/votacion/votacion.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Votacion1Component } from './votacion1/votacion1.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [Votacion1Component]
+})
+export class VotacionModule {}
