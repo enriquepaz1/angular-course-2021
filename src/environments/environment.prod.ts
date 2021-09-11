0031700ae4faa32a823f228ab7597f5b845ba092
enriquepaz1 <>
2021-09-11 02:53:45 -0400

    task2:10/09/21

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  app:{
+   apiBaseUrl: "https://facebook-angular-enrique-default-rtdb.firebaseio.com"
+  },
+  auth:{
+     apiBaseUrl:"https://identitytoolkit.googleapis.com",
+     key: "AIzaSyC_j416I-hpjIXmq6N-5w7ZiDjNAc5NC-0"
+
+  }
 };
