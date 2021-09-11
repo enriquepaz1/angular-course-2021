7efe2761b2319a106ee7518c8285929f975ead16
enriquepaz1 <>
2021-09-11 02:55:26 -0400

    task3: 10/09/21

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,109 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { PublicationService } from './services/publication.service';
-import { SingletonService } from './services/singleton.service';
-import { Test1Service } from './services/test1.service';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { AuthService } from './services/auth.service';
+import {MatDialog} from "@angular/material/dialog"
+import { RegisterComponent } from './components/register/register.component';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
-
-
 export class LoginComponent implements OnInit {
-  name1:string =  '';
-   name = new FormControl('');
-   formReactive: FormGroup;
-
-  constructor(private test1Service:Test1Service,private singletonService:SingletonService,
-    private publicationService:PublicationService, private formBuilder: FormBuilder) {
-    console.log(this.test1Service.getItems)
-
-    this.formReactive = this.formBuilder.group({
-    // name:'',
-     //lastName:['',[Validators.required]],
-     //date:''
-     name:['',[Validators.required,Validators.minLength(3)]],
-     lastName:''
 
 
+  constructor(private authService:AuthService, private matDialog:MatDialog, private router:Router) {
 
-    });
-    }
-
-
-
-  ngOnInit():void {
-
-    this.formReactive.valueChanges.subscribe(res=>{
-        console.log('FORM REACTIVE', res);
-    });
-
-
-    this.name.valueChanges.subscribe(res=>{
-      console.log('CHANGES', res)
-    });
-
-
-   this.publicationService.getAll().subscribe(res=>{
-     console.log('RESPONSE: ', res);
-   });
 
   }
 
-  getValue(value:string){
-    return this.formReactive.get(value);
+  ngOnInit(): void {
+  }
+  login(form: any){
+console.log('AAAAA',form.value);
+
+  this.authService.login({
+     email:form.value.email,
+     password:form.value.password,
+     returnSecureToken: true
+
+  }).subscribe(res=>{
+    console.log('RESPONSE',res);
+    this.router.navigate(['pages']);
+  });
+
   }
 
-  setMessage():void{
-    this.singletonService.setMessage('Hi from Login');
+  onCreateNewAccount(){
+    this.matDialog.open(RegisterComponent)
   }
-  create(){
-    this.publicationService.create({
-      "date": "17/82/2021",
-      "description": "soy nuevo",
-      "idUser": 2,
-      "imageUrl": "http://fitebase/img.jpg",
-      "userPhotoUrl": "http://fitebase/user1.jpg"
-    } ).subscribe(
-      res => console.log(res))
-  }
-  delete(){
-    this.publicationService.delete('p0002').subscribe(
-      res => console.log(res))
-  }
-  update(){
-    this.publicationService.update('p0001',{
-      "date": "17/82/2021",
-      "description": "update from frontend",
-      "idUser": 2,
-      "imageUrl": "http://fitebase/img.jpg",
-      "userPhotoUrl": "http://fitebase/user1.jpg"
-    } ).subscribe(
-      res => console.log(res))
-  }
-
-
-
-  patch(){
-    this.publicationService.patch('p0001',{
-      "description": "update from frontend patch"
-    } ).subscribe(
-      res => console.log(res))
-  }
-
-  onSubmitTemplate(values: any){
-  console.log('VALUES:', values)
-  }
-
-  onShow(){
-    console.log(this.name.value)
-  }
-
-   onShowAll(){
-     console.log('DDDD:' , this.formReactive.value)
-   }
 
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbButtonsModule, NgbModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgbButtonsModule, NgbToastModule} from "@ng-bootstrap/ng-bootstrap";
 import {MatSliderModule} from "@angular/material/slider";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import { Test1Service } from './services/test1.service';
+import {Test1Service} from "./services/test1.service";
+import {SingletonService} from "./services/singleton.service";
 import { Login1Component } from './components/login1/login1.component';
 import { Login2Component } from './components/login2/login2.component';
-import { UtilsService } from './services/utils.service';
-import { PublicationService } from './services/publication.service';
+import {UtilsService} from "./services/utils.service";
+import {PublicationService} from "./services/publication.service";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import { RegisterComponent } from './components/register/register.component';
+
 
 const routes:Routes=[
   {path:'',component:LoginComponent}
@@ -19,16 +25,19 @@ const routes:Routes=[
 
 @NgModule({
   imports: [
+    MatProgressSpinnerModule,
     NgbToastModule,
     NgbButtonsModule,
     MatSliderModule,
-    MatProgressSpinnerModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
-  declarations: [LoginComponent,Login1Component,Login2Component],
+  declarations: [LoginComponent,Login1Component,Login2Component, RegisterComponent],
 
   providers:[Test1Service,UtilsService,PublicationService]
 
