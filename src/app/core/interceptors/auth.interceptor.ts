aca1b89ee1832c7d46bc9c2f60f6ca6d037ea2ce
enriquepaz1 <>
2021-09-11 02:56:16 -0400

    task4: 10/09/21

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { AuthService } from '../../login/services/auth.service';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService:AuthService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const token = this.authService.getToken();
+
+    if(token){
+      //let params = new HttpParams();
+      //params.append('auth', 'asdasdasdas')
+      //params.append('dgdfg', 'asdasdasdas')
+      //params.append('aughkhjkhjkth', 'asdasdasdas')
+
+      //console.log('PARAMS', params)
+
+      request = request.clone({
+        url: `${request.url}?auth=${token}`
+      });
+    }
+
+    return next.handle(request).pipe(
+      catchError( (err:any) => {
+        console.log('ERROR', err)
+        return throwError('ERROR EXTRA')
+      })
+    );
+  }
+
+}
