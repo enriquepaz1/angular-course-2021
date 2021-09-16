ce3b0290bf91c35075fb27dfd383737ab5734bdb
enriquepaz1 <enrique.bayron.paz@gmail.com>
2021-09-16 18:02:35 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BilleteraService } from './billetera.service';
+import { BilleteraComponent } from './billetera/billetera.component';
 import { TransaccionService } from './transaccion.service';
+import { TransaccionComponent } from './transaccion/transaccion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BilleteraComponent,
+    TransaccionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule
   ],
   providers: [TransaccionService,BilleteraService],
 
