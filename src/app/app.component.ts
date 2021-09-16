e3bf094f8e599bea6c2b26fda6e66e0b3f0b0d60
enriquepaz1 <enrique.bayron.paz@gmail.com>
2021-09-16 17:49:07 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BilleteraService } from './billetera.service';
+import { TransaccionService } from './transaccion.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular2021';
+  wallets: any[] = [];
+  transactions: any[] = [];
+
+  constructor(
+    private transaccionService: TransaccionService,
+    private billeteraService: BilleteraService
+  ) {}
+
+  ngOnInit() {
+    this.getTransaccion();
+    this.getWallets();
+  }
+
+  getTransaccion() {
+    this.transaccionService.getAll().subscribe(
+      (res) =>
+        (this.transactions = Object.entries(res).map((s: any) => ({
+          id: s[0],
+          ...s[1],
+        })))
+    );
+  }
+
+  getWallets() {
+    this.billeteraService.getAll().subscribe(
+      (res) =>
+        (this.wallets = Object.entries(res).map((s: any) => ({
+          id: s[0],
+          ...s[1],
+        })))
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BilleteraService } from './billetera.service';
+import { TransaccionService } from './transaccion.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TransaccionService,BilleteraService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
